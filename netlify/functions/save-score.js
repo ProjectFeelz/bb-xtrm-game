1c9d0e1e1f79616dd0cdb23bb3c560c726637cb8
ProjectFeelz <steve@projectfeelz.com>
2026-01-01 18:18:09 +0200

    save-score.js

---
+++ netlify/functions/save-score.js
@@ -0,0 +1,17 @@
+const { createClient } = require('@supabase/supabase-client');
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+exports.handler = async (event) => {
+  const { username, score } = JSON.parse(event.body);
+
+  await supabase
+    .from('leaderboard')
+    .insert([{ username: username, clout: score }]);
+
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify({ message: "Score Saved" })
+  };
+};
